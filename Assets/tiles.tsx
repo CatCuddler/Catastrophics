7dd46cf05eaa3d8110a8f81197a957d40023e097
Polona Caserman <polona.caserman@gmail.com>
2018-12-01 20:04:27 +0100

    Add new assets

--- Assets/tiles.tsx
+++ Assets/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tiles" tilewidth="128" tileheight="168" tilecount="32" columns="32">
- <image source="../Deployment/Tiles/room.png" width="4096" height="168"/>
+ <image source="../Deployment/Tiles/tiles.png" width="4096" height="168"/>
 </tileset>
